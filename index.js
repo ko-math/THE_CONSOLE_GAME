3065f9a0bbf4ad3e0af72a556caf415f147327fb
ko-math <sorehakiemasu@gmail.com>
2026-09-01 19:41:37 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+const b = document.querySelector('#b');
+b.addEventListener('click',()=>{
+  const i = document.querySelector('#i').value;
+  location.href = 'https://ko-math.github.io/THE_CONSOLE_GAMES/' + i;
+});
